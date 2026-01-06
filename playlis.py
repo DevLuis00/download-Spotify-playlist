2ff634bd1306aaf31af1cc43b49ac7aac2feaedb
rosalesv <devluisrosales@gmail.com>
2026-01-05 23:26:31 -0500

    Update playlis.py

--- playlis.py
+++ playlis.py
@@ -1,3 +1,4 @@
+#Linux
 import os
 
 playlist_url = input("Playlist URL:")
@@ -7,4 +8,20 @@ os.makedirs(output_folder, exist_ok=True)
 
 command = f'spotdl "{playlist_url}" --output "{output_folder}"' 
 os.system(command)
+# pip install spotdl
+# source ~/venv-spotdl/bin/activate
 
+
+#Windows
+import os
+
+playlist_url = input("Playlist URL: ")
+
+output_folder = os.path.expanduser("~/Music/Prueba")
+os.makedirs(output_folder, exist_ok=True)
+
+command = f'python -m spotdl "{playlist_url}" --output "{output_folder}"'
+os.system(command)
+
+# python -m pip install spotdl
+# winget install Gyan.FFmpeg
